94a4b957ba8fe888ca4a48f9e5d8083f96133a8d
Sachin Kainth <Sachin Kainth>
2016-12-27 01:06:19 +0000

    Added exception handling

--- DFD/Program.cs
+++ DFD/Program.cs
@@ -6,23 +6,30 @@ namespace DFD
     {
         static void Main(string[] args)
         {
-            var duplicateFileDisplayer = new DuplicateFileDisplayer(args, new ConsoleWrapper());
-            var duplicateFileDeleter = new DuplicateFileDeleter(new FileWrapper());
+            try
+            {
+                var duplicateFileDisplayer = new DuplicateFileDisplayer(args, new ConsoleWrapper());
+                var duplicateFileDeleter = new DuplicateFileDeleter(new FileWrapper());
 
-            var hashes1 = new DirectoryHasher(new DirectoryWrapper(), args[0]).ComputeHashes();
-            var hashes2 = new DirectoryHasher(new DirectoryWrapper(), args[1]).ComputeHashes();
+                var hashes1 = new DirectoryHasher(new DirectoryWrapper(), args[0]).ComputeHashes();
+                var hashes2 = new DirectoryHasher(new DirectoryWrapper(), args[1]).ComputeHashes();
 
-            duplicateFileDisplayer.Display(hashes1);
-            duplicateFileDisplayer.Display(hashes2);
+                duplicateFileDisplayer.Display(hashes1);
+                duplicateFileDisplayer.Display(hashes2);
 
-            duplicateFileDeleter.Delete(hashes1);
-            duplicateFileDeleter.Delete(hashes2);
+                duplicateFileDeleter.Delete(hashes1);
+                duplicateFileDeleter.Delete(hashes2);
 
-            duplicateFileDisplayer.Display(hashes1, hashes2);
+                duplicateFileDisplayer.Display(hashes1, hashes2);
 
-            duplicateFileDeleter.Delete(hashes1, hashes2);
+                duplicateFileDeleter.Delete(hashes1, hashes2);
 
-            Console.Read();
+                Console.Read();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
